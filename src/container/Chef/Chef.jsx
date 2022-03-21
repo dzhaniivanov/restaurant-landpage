cbcd7010e2eeb4bb9b160a0146196ea19f9b3f42
dzhani ivanov <djani.ivanov94@gmail.com>
2022-03-21 13:36:02 +0200

    chef page layout added

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
-
-import './Chef.css';
+import React from "react";
+import { images } from "../../constants";
+import "./Chef.css";
+import { SubHeading } from "../../components";
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde consectetur dicta a commodi. Mollitia, repellat impedit quas illum soluta tenetur!</p>
+        </div>
+        <p className="p__opensans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla nam sint vitae illo aperiam mollitia ad porro numquam suscipit cum.</p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+
+    </div>
   </div>
 );
 
